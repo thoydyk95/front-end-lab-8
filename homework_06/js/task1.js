9a6d70a69b61f32583da7584b252ac3c0fffe0d4
Tetiana Hoidyk <thoydyk@gmail.com>
2018-02-11 21:27:00 +0200

    Update task1.js

--- homework_06/js/task1.js
+++ homework_06/js/task1.js
@@ -1,7 +1,7 @@
 window.onload = function() {
-    var a = parseFloat(prompt("First side of tiangle is:", "0"));
-    var b = parseFloat(prompt("Second side of tiangle is:", "0"));
-    var c = parseFloat(prompt("Third side of tiangle is:", "0"));
+    var a = parseFloat(prompt("First side of tiangle is:", '0'));
+    var b = parseFloat(prompt("Second side of tiangle is:", '0'));
+    var c = parseFloat(prompt("Third side of tiangle is:", '0'));
     var p = (a + b + c) / 2;
     var s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
     var teoremPifag = Math.pow(a, 2) == Math.pow(b, 2) + Math.pow(c, 2) || Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2) || Math.pow(a, 2) == Math.pow(c, 2) + Math.pow(a, 2);
