8ef632048816fded69854e3d3f10043e34b70b6b
Tetiana Hoidyk <thoydyk@gmail.com>
2018-02-13 21:00:08 +0200

    correct task2.js

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -10,33 +10,31 @@ if (playGame === true) {
     for (let i = 0; i < 3; i++) {
         curPrize = 10 * multiplic;
         switch (attempt) {
-        case 2:
-            curPrize = parseInt(5 * multiplic);
-            break;
-        case 1:
-            curPrize = parseInt(5 / 2 * multiplic);
-            break;
+            case 2:
+                curPrize = parseInt(5 * multiplic);
+                break;
+            case 1:
+                curPrize = parseInt(5 / 2 * multiplic);
+                break;
         }
         userString = prompt('Enter a number from 0 to ' + max + '\nAttempts left: ' + attempt + '\nTotal prize: ' + totalPrize + '$' + '\nPossible prize on current attempt: ' + curPrize + '$', '0');
         if (userString == n) {
             switch (attempt) {
-            case 3:
-                totalPrize += 10 * multiplic;
-                break;
-            case 2:
-                totalPrize += parseInt(5 * multiplic);
-                break;
-            case 1:
-                totalPrize += parseInt(5 / 2 * multiplic);
-                break;
+                case 3:
+                    totalPrize += 10 * multiplic;
+                    break;
+                case 2:
+                    totalPrize += parseInt(5 * multiplic);
+                    break;
+                case 1:
+                    totalPrize += parseInt(5 / 2 * multiplic);
+                    break;
             }
             playGame = confirm('Congrats, do you want to continue the game?');
             if (playGame == false) {
                 console.log('Thank you for a game. Your prize is : ' + totalPrize);
                 playGame = confirm('Do you wanna play again?');
-                if (playGame == false) {
-                    break;
-                } else {
+                if (playGame == true) {
                     n = Math.floor(Math.random() * (max + 1));
                     totalPrize = 0;
                     i = -1;
@@ -44,6 +42,8 @@ if (playGame === true) {
                     multiplic = 1;
                     attempt = 3;
                     continue;
+                } else {
+                    break;
                 }
             } else {
                 n = Math.floor(Math.random() * (max + 1));
